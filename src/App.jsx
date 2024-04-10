31a0b9d1abcd36227fe5d82a3c77844cd2b1f09c
Katiuska Alicea <katiuskaalicea@gmail.com>
2024-04-10 16:15:27 -0700

    Update App.jsx and tailwind.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="flex w-full">
+      <div className="card w-96 bg-base-100 shadow-xl mx-auto mt-8">
+        <div className="card-body">
+          <h2 className="card-title text-center">Vite + TailwindCSS + DaisyUI</h2>
+          <p>Are Installed and Ready</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Click Me!</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
